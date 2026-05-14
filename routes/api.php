60c3c6fa804d03cee4f5f41d3dc6b62358c5098f
fienda87 <fienda87@users.noreply.github.com>
2026-05-14 14:57:36 +0000

    Implement API endpoints, resources, and controllers. Installed Laravel Breeze for authentication.

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\CategoryController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/articles', [ArticleController::class, 'index']);
+Route::get('/articles/{slug}', [ArticleController::class, 'show']);
+Route::get('/categories', [CategoryController::class, 'index']);
