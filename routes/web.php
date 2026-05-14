cc19ff45334ccb33d5b75e87fdd38c18e56c8804
fienda87 <fienda87@users.noreply.github.com>
2026-05-14 14:57:36 +0000

    Implement API endpoints, resources, and controllers. Installed Laravel Breeze for authentication.

--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,27 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use Illuminate\Foundation\Application;
 
 Route::get('/', function () {
-    return Inertia::render('Home');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
-Route::get('/about', function () {
-    return Inertia::render('About');
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/program', function () {
-    return Inertia::render('Program');
-});
-
-Route::get('/perjalanan', function () {
-    return Inertia::render('Perjalanan');
-});
-
-Route::get('/dampak', function () {
-    return Inertia::render('Dampak');
-});
-
-Route::get('/galeri', function () {
-    return Inertia::render('Galeri');
-});
-
-Route::get('/kunjungi-kami', function () {
-    return Inertia::render('KunjungiKami');
-});
+require __DIR__.'/auth.php';
